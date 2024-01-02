5c6815850547842bf64d535e279c06fe0957a4fa
dell <mirek.stranskz@gmail.com>
2024-01-02 22:06:18 +0100

    Load new project called Houzvickovacz

---
+++ src/main/java/cz/houzvickova/houzvickovacz/HouzvickovaczApplication.java
@@ -0,0 +1,13 @@
+package cz.houzvickova.houzvickovacz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HouzvickovaczApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HouzvickovaczApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/houzvickova/houzvickovacz/HouzvickovaczApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.houzvickova.houzvickovacz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouzvickovaczApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
